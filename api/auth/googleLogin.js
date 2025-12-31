53a04556c9bf23eb3ec8174299cfcb38761d24e1
Ahrar Shah <ahrar.09321@gmail.com>
2026-01-01 00:06:59 +0500

    CORS Fixed!

--- api/auth/googleLogin.js
+++ api/auth/googleLogin.js
@@ -1,6 +1,24 @@
 import { createAppwriteClient } from "../config.js";
 
 export default async function handler(req, res) {
+
+    const allowedOrigins = [
+        "https://au-ride.vercel.app",
+        "http://127.0.0.1:5500",
+        "http://localhost:5500"
+    ];
+
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader("Access-Control-Allow-Origin", origin);
+    }
+
+    res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+    if (req.method === "OPTIONS") {
+        return res.status(200).end();
+    }
     try {
         const { auth } = await createAppwriteClient();
 
