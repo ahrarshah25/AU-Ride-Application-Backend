223cba30cdbf7fb0bbeb45206ef2a87827e21245
Ahrar Shah <ahrar.09321@gmail.com>
2026-01-03 00:07:52 +0500

    Get Fixed Check

--- api/profile/get.js
+++ api/profile/get.js
@@ -12,7 +12,7 @@ export default async function loadProfile(req, res) {
         res.setHeader("Access-Control-Allow-Origin", origin);
     }
 
-    res.setHeader("Access-Control-Allow-Methods", "POST,OPTIONS");
+    res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     res.setHeader("Access-Control-Allow-Credentials", "true");
 
