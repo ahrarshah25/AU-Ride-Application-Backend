5bbdc431d8c0d4e957b05b989ea8e745befb7fd7
Muhammad Mustafa Arman <m.mustafaarman2010@gmail.com>
2026-01-02 16:11:33 +0500

    Fixed!

--- api/auth/googleLogin.js
+++ api/auth/googleLogin.js
@@ -1,22 +1,22 @@
 export default function handler(req, res) {
   const projectId = "6952bae80027e91db515";
 
-  // const success = encodeURIComponent(
-  //   "https://au-ride.vercel.app/signup?status=success"  
-  // );
-
-  // const failure = encodeURIComponent(
-  //   "https://au-ride.vercel.app/signup?status=failed"
-  // );
-
-
   const success = encodeURIComponent(
-    "https://localhost:5500/auth/signup.html?status=success"  
+    "https://au-ride.vercel.app/signup?status=success"  
   );
 
   const failure = encodeURIComponent(
-    "https://localhost:5500/auth/signup.html?status=failed"
+    "https://au-ride.vercel.app/signup?status=failed"
   );
+
+
+  // const success = encodeURIComponent(
+  //   "https://localhost:5500/auth/signup.html?status=success"  
+  // );
+
+  // const failure = encodeURIComponent(
+  //   "https://localhost:5500/auth/signup.html?status=failed"
+  // );
 
   const oauthUrl =
     `https://fra.cloud.appwrite.io/v1/account/sessions/oauth2/google` +
