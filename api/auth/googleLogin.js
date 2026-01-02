a1f6084d6faff935da5ed59f9d228acce39ebe2c
Ahrar Shah <ahrar.09321@gmail.com>
2026-01-02 13:07:18 +0500

    Try To Fix AppWrite Error!

--- api/auth/googleLogin.js
+++ api/auth/googleLogin.js
@@ -1,12 +1,21 @@
 export default function handler(req, res) {
   const projectId = "6952bae80027e91db515";
 
+  // const success = encodeURIComponent(
+  //   "https://au-ride.vercel.app/signup?status=success"  
+  // );
+
+  // const failure = encodeURIComponent(
+  //   "https://au-ride.vercel.app/signup?status=failed"
+  // );
+
+
   const success = encodeURIComponent(
-    "https://au-ride.vercel.app/signup?status=success"  
+    "https://localhost:5500/auth/signup.html?status=success"  
   );
 
   const failure = encodeURIComponent(
-    "https://au-ride.vercel.app/signup?status=failed"
+    "https://localhost:5500/auth/signup.html?status=failed"
   );
 
   const oauthUrl =
